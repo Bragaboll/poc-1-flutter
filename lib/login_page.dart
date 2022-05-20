93497ace1cac0b1cb5e10e448b1cb04a41d72753
Bragaboll <bragaboll@hotmail.com>
2022-05-20 12:02:42 -0300

    Card modifucado

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,88 +1,136 @@
+import 'package:app_treino/switch_widget.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  String email = '';
-  String senha = '';
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-            child: Text(
-          'Login',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        )),
-      ),
-      backgroundColor: Colors.purple[100],
-      body: SingleChildScrollView(
+      drawer: Drawer(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            UserAccountsDrawerHeader(
+                currentAccountPicture: ClipOval(
+                  child: Image.asset('assets/br.jpg'),
+                ),
+                accountName: const Text('Luiz Braga'),
+                accountEmail: const Text('luiz@hotmail.com')),
             Padding(
-              padding: const EdgeInsets.only(top: 60),
-              child: Center(child: Image.asset('assets/logo.png')),
-            ),
-            Card(
+              padding: const EdgeInsets.all(10),
               child: Column(
                 children: [
-                  TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    onChanged: (text) {
-                      email = text;
-                    },
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      labelStyle: TextStyle(
-                          color: Colors.purple,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                      border: OutlineInputBorder(),
+                  ListTile(
+                    leading: const Icon(Icons.home),
+                    trailing: const Text(
+                      'Menu',
+                      style: TextStyle(fontSize: 18),
                     ),
-                  ),
-                  Container(
-                    height: 10,
-                    color: Colors.purple[100],
-                  ),
-                  TextField(
-                    onChanged: (text) {
-                      senha = text;
+                    onTap: () {
+                      Navigator.of(context).pop('/home');
                     },
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      labelText: 'Senha',
-                      labelStyle: TextStyle(
-                          color: Colors.purple,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                      border: OutlineInputBorder(),
-                    ),
                   ),
                 ],
               ),
             ),
-            const SizedBox(
-              height: 50,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (email == 'luizao@hotmail.com' && senha == '123') {
-                  Navigator.of(context).pushReplacementNamed('/home');
-                }
-              },
-              child: const Text(
-                'Entrar',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
-                ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.home),
+                    trailing: const Text(
+                      'Sair',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushReplacementNamed('/');
+                    },
+                  ),
+                ],
               ),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        // ignore: prefer_const_literals_to_create_immutables
+        actions: [
+          const SwitchWidget(),
+        ],
+        title: const Center(
+          child: Text(
+            'LovePepole',
+            style: TextStyle(fontSize: 25),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 60),
+              child: Center(
+                child: Image.asset('assets/logo.png'),
+              ),
+            ),
+            const Text(
+              'Voce esta na LovePeople',
+              style: TextStyle(
+                  fontSize: 27,
+                  color: Colors.purple,
+                  fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              'Seja muito bem vindo!!!',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.purple,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 90,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.purple),
+                      child: Image.asset('assets/cor.png'),
+                    ),
+                    Container(
+                      height: 60,
+                      width: 90,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.purple),
+                      child: Image.asset('assets/cor.png'),
+                    ),
+                    Container(
+                      height: 60,
+                      width: 90,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.purple),
+                      child: Image.asset('assets/cor.png'),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+              ],
             ),
           ],
         ),
